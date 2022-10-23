1587fbe10f180215c0d13f2c1081f2b95563cc00
maxim arestov <aresmax1993@gmail.com>
2022-10-23 11:01:12 +0300

    Complete Case 3

--- Case3/Program.cs
+++ Case3/Program.cs
@@ -2,9 +2,28 @@
 // 4->24 5->120
 Console.Clear();
 Console.WriteLine("Введите число: ");
-bool checkDigit = int.TryParse(Console.ReadLine(), out int a);
+bool checkDigit = int.TryParse(Console.ReadLine(), out int n);
 if (!checkDigit)
 {
     Console.WriteLine("Введены некорректные данные: ");
     return;
+}
+if (n < 1)
+{
+    Console.WriteLine("-0");
+}
+Console.WriteLine($"Произведение чисел от 1 до N равно {GetDigitPlenty(n)}");
+
+
+
+
+
+int GetDigitPlenty(int number)
+{
+    int plenty = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        plenty *= i;
+    }
+    return plenty;
 }
