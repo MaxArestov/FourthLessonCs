31b4b0d6b7af789b9a41048dcbb58dd2f1545578
maxim arestov <aresmax1993@gmail.com>
2022-10-23 10:35:27 +0300

    Added Case 2

--- Case1/Program.cs
+++ Case1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28 4 -> 10 8 -> 36
+Console.Clear();
+bool isParsed = int.TryParse(Console.ReadLine(), out int a);
+if (!isParsed)
+{
+    Console.WriteLine("Введены некорректные данные.");
+}
+int sum = GetSumNumbers(a);
+Console.WriteLine(sum);
+int GetSumNumbers(int number)
+{
+    int sum = 0;
+    for (int i = 0; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
---
+++ Case2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3 78 -> 2 89126 -> 5
+
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+bool checkDigit = int.TryParse(Console.ReadLine(), out int a);
+if (!checkDigit)
+{
+    Console.WriteLine("Введены некорректные данные: ");
+    return;
+}
+// Console.WriteLine(DigitCount(a));
+Console.WriteLine(DigitCount1(a));
+
+
+int DigitCount(int num)
+{
+    int count = 1;
+    while ((num / 10) != 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+int DigitCount1(int number)
+{
+    int i = 0;
+    for (i = 0; i <=number; i++)
+    {
+        number = number / 10;
+    }
+    return i;
+}
